426d8048ee7d50c5c32896839c9c01aca677a6ae
Eduardo Martínez <soka_117@live.com>
2026-08-14 14:30:41 -0600

    feat: we started implementing the Repository Pattern in our project to separate the business logic from the data access logic

--- src/main/java/edd/dev/main/Main.java
+++ src/main/java/edd/dev/main/Main.java
@@ -1,16 +1,14 @@
 package edd.dev.main;
 
+import edd.dev.util.DataBaseConnection;
+
 import java.sql.*;
 
 public class Main {
     static void main(String[] args) throws SQLException {
-        final String URL = "";
-        final String USER = "";
-        final String PASSWORD = "";
-
-        try (Connection myConnection = DriverManager.getConnection(URL, USER, PASSWORD);
+        try (Connection myConnection = DataBaseConnection.getInstance();
              Statement myStatement = myConnection.createStatement();
-             ResultSet myResultSet = myStatement.executeQuery("SELECT * FROM employees");) { // try with resources, better than finally
+             ResultSet myResultSet = myStatement.executeQuery("SELECT * FROM employees");) { // try-with-resources, better than finally
 
             while (myResultSet.next()) {
                 System.out.println(myResultSet.getString("first_name"));
@@ -19,16 +17,5 @@ public class Main {
             e.printStackTrace();
             System.out.println("No se pudo establecer la conexión...");
         }
-        /* finally {
-            if (myResultSet != null) {
-                myResultSet.close();
-            }
-            if (myStatement != null) {
-                myResultSet.close();
-            }
-            if (myConnection != null) {
-                myResultSet.close();
-            }
-        }*/
     }
 }
---
+++ src/main/java/edd/dev/repository/repository.java
@@ -0,0 +1,4 @@
+package edd.dev.repository;
+
+public interface repository {
+}
